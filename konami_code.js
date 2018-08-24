f785a96e2e676da1d70ce4813454216b1307f947
Amanda Thomas <amandamariathomas@gmail.com>
2018-08-24 00:39:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,32 +1,32 @@
-// window.onload = function (e){
-//   console.log("window.onload");
-//
-//
-//   const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-//
-//   function init() {
-//     // Write your JavaScript code inside the init() function
-//     console.log("== init ==");
-//
-//     let bodyArray = document.getElementsByTagName("body");
-//     console.log("bodyArray:", bodyArray);
-//
-//     bodyArray[0].addEventListener("keydown", onKeyDownHandler);
-//
-//     function onKeyDownHandler(e) {
-//       console.log('== onKeyDownHandler ==');
-//       console.log("e.which", e.which);
-//
-//       if (e.which === 37) {
-//         console.log("37");
-//       } else if (e.which === 38) {
-//         console.log("38");
-//       } else if (e.which === 39) {
-//         console.log("39");
-//       } else if (e.which === 40) {
-//         console.log("40");
-//       }
-//     }
-//     }
-//   init();
-// }
+window.onload = function (e){
+  console.log("window.onload");
+
+
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+  function init() {
+    // Write your JavaScript code inside the init() function
+    console.log("== init ==");
+
+    let bodyArray = document.getElementsByTagName("body");
+    console.log("bodyArray:", bodyArray);
+
+    bodyArray[0].addEventListener("keydown", onKeyDownHandler);
+
+    function onKeyDownHandler(e) {
+      console.log('== onKeyDownHandler ==');
+      console.log("e.which", e.which);
+
+      if (e.which === 37) {
+        console.log("37");
+      } else if (e.which === 38) {
+        console.log("38");
+      } else if (e.which === 39) {
+        console.log("39");
+      } else if (e.which === 40) {
+        console.log("40");
+      }
+    }
+    }
+  init();
+}
